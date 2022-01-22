2d80c8185eef21d9c6b909013064c6f921d98638
Alexandru <alexader_alexandruscrob@yahoo.com>
2022-01-22 21:00:59 +0200

    Added user model WIP

--- app/main.py
+++ app/main.py
@@ -1,11 +1,28 @@
-import config
+import db
 from fastapi import FastAPI
+from cassandra.cqlengine.management import sync_table
+from users.models import User
 
 
 app = FastAPI()
-settings = config.get_settings()
+
+
+DB_SESSION = None
+
+
+@app.on_event("startup")
+def on_startup():
+    global DB_SESSION
+    DB_SESSION = db.get_session()
+    sync_table(User)
 
 
 @app.get("/")
 def homepage():
     return {"hello": "world"}
+
+
+@app.get("/users")
+def users_list_view():
+    q = User.objects.all().limit(10)
+    return list(q)
---
+++ app/users/models.py
@@ -0,0 +1,39 @@
+import uuid
+
+import validators
+from config import get_settings
+
+from cassandra.cqlengine import columns
+from cassandra.cqlengine.models import Model
+
+
+settings = get_settings()
+
+
+class User(Model):
+    __keyspace__ = settings.keyspace
+    email = columns.Text(primary_key=True)
+    user_id = columns.UUID(primary_key=True, default=uuid.uuid1)
+    password = columns.Text()
+
+    def __str__(self):
+        return self.__repr()
+
+    def __repr__(self):
+        return f"User(email={self.email}, user_id={self.user_id})"
+
+    @staticmethod
+    def create_user(email, password=None):
+        q = User.objects.filter(email=email)
+        if q.count() != 0:
+            raise Exception("User already has account.")
+
+        valid, msg, email = validators._validate_email(email)
+
+        if not valid:
+            raise Exception(f"Invalid email: {msg}")
+
+        obj = User(email=email)
+        obj.password = password
+        obj.save()
+        return obj
